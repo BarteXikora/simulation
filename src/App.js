747adf55661f7fb0e9f3f64f255d01bbe69b773e
Bartłomiej Sikora <sbartek50@gmail.com>
2023-04-04 18:50:01 +0200

    Created styles for canvas, set background to black.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 
+import './style.css'
+
 const App = () => {
   return (
     <Canvas>
-
+      <color args={[0, 0, 0]} attach={'background'} />
     </Canvas>
   )
 }
