d859330c88749a3f2ed73ca9a0f335636ce0cc69
Bartłomiej Sikora <sbartek50@gmail.com>
2023-04-05 19:59:32 +0200

    Prepared scene to test collisions.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,23 @@ const App = () => {
     <Canvas>
       <color args={[0, 0, 0]} attach={'background'} />
 
-      <Physics gravity={{ x: 0, y: -10 }} airResistance={8}>
+      <Physics gravity={{ x: 0, y: -1 }} airResistance={8}>
         <Dynamic
           type='ball'
           element={<mesh>
             <sphereGeometry args={[0.2]} />
             <meshBasicMaterial color={'red'} />
           </mesh>}
-          config={{ startPosition: { x: 3, y: 0 } }}
+          config={{ startPosition: { x: 0, y: 2 } }}
+        />
+
+        <Dynamic
+          type='ball'
+          element={<mesh>
+            <sphereGeometry args={[0.2]} />
+            <meshBasicMaterial color={'blue'} />
+          </mesh>}
+          config={{ startPosition: { x: 0, y: -2 }, startVelocity: { x: 0, y: 0.09 } }}
         />
       </Physics>
     </Canvas>
