8a298b65f37f5e191592f04b950e5eb821fb6762
Bartomiej Sikora <sbartek50@gmail.com>
2023-04-06 09:37:00 +0200

    Modified scene to test balls behavior.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ const App = () => {
     <Canvas>
       <color args={[0, 0, 0]} attach={'background'} />
 
-      <Physics gravity={{ x: 0, y: -1 }} airResistance={8}>
+      <Physics gravity={{ x: 0, y: 0 }} airResistance={8}>
         <Dynamic
           type='ball'
           element={<mesh>
             <sphereGeometry args={[0.2]} />
             <meshBasicMaterial color={'red'} />
           </mesh>}
-          config={{ startPosition: { x: 0, y: 2 }, radius: 0.2 }}
+          config={{ startPosition: { x: 1, y: 0 }, startVelocity: { x: -0.005, y: 0 }, radius: 0.2 }}
         />
 
         <Dynamic
@@ -27,7 +27,7 @@ const App = () => {
             <sphereGeometry args={[0.2]} />
             <meshBasicMaterial color={'blue'} />
           </mesh>}
-          config={{ startPosition: { x: 0, y: -2 }, startVelocity: { x: 0, y: 0.09 }, radius: 0.2 }}
+          config={{ startPosition: { x: 0, y: -0.75 }, startVelocity: { x: 0.02, y: 0.02 }, radius: 0.2 }}
         />
       </Physics>
     </Canvas>
